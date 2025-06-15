347a4737bb7edcf271d470b5958558ccf2a2dec9
Carter <carterinmotion@gmail.com>
2025-06-14 19:56:44 -0600

    Updates

---
+++ assets/pom-DiIMUTy1.js
@@ -0,0 +1,7 @@
+import{j as n}from"./index-DZENqHY2.js";function o(t){const e={h1:"h1",p:"p",strong:"strong",...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h1,{id:"pomodoro-timer",children:"Pomodoro Timer"}),`
+`,n.jsx(e.p,{children:n.jsx(e.strong,{children:"Type"})}),`
+`,n.jsx(e.p,{children:"Webapp"}),`
+`,n.jsx(e.p,{children:n.jsx(e.strong,{children:"Use"})}),`
+`,n.jsx(e.p,{children:"Build intention and sustainabiliy into your mental focus using this timer."}),`
+`,n.jsx(e.p,{children:n.jsx(e.strong,{children:"Time spent building"})}),`
+`,n.jsx(e.p,{children:"You don't wanna know"})]})}function r(t={}){const{wrapper:e}=t.components||{};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{r as default};
