871afe3fd5316f6f90c8b4015fc16cca22414acc
Carter <carterinmotion@gmail.com>
2025-10-29 21:16:45 +0000

    Inital fibre commit

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,36 @@
+import { useEffect, useRef, useState } from 'react';
+import * as THREE from 'three';
+import {Canvas, useFrame} from '@react-three/fiber'
+
+function Box(props) {
+				const meshRef = useRef()
+				const [hovered, setHover] = useState(false)
+				const [active, setActive] = useState(false)
+				useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+
+				return (
+								<mesh
+  								{...props}
+  								ref={meshRef}
+  								scale={active ? 1.5 : 1}
+  								onClick={() => setActive(!active)}
+  								onPointerOver={() => setHover(true)}
+  								onPointerOut={() => setHover(false)}
+								>
+  								<boxGeometry args={[1,1,1]} />
+  								<meshStandardMaterial color={hovered ? 'red': 'blue'} />
+								</mesh>
+				)
+}
+
 export default function Home() {
 	return (
 		<>
 			<div id='bigname' style={{ display: 'flex', width: '100%', flexGrow: 1, justifyContent: 'center', alignItems: 'start', flexDirection: 'column', paddingLeft: '30%' }}>
-
-				<h1>Carter</h1>
-				<h1>James</h1>
-				<h1>Andrew</h1>
-				<text>Human in progress</text>
-				<text>Based In Boulder</text>
-			</div>
-			<div>
-				<span>
-					<p>This website is being re-vamped and still under construction. Thank you for your patience!</p>
-				</span>
+				<Canvas>
+				  <ambientLight intensity={Math.PI / 2} />
+					<Box position={[0,0,0]} />
+				</Canvas>
 			</div>
 		</>
 	)
