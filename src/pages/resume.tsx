4460f59564d5104e5c42d4f41ceaf0f8cfec8b94
Carter <carterinmotion@gmail.com>
2025-10-29 20:26:17 +0000

    Not sure

---
+++ src/pages/resume.tsx
@@ -0,0 +1,14 @@
+export default function Resume(){
+		return (
+				<object
+				  data='https://raw.githubusercontent.com/carterjandrew/resume/refs/heads/main/index.pdf'
+				  type='application/pdf'
+				  width='100%'
+				  height='100%'
+				>
+				  <p> Unable to display PDF file.
+				   <a href="https://raw.githubusercontent.com/carterjandrew/resume/refs/heads/main/index.pdf">Open here</a>
+		          </p>
+				</object>
+		)
+}
