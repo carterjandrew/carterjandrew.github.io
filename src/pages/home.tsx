92648c2d3e437d5fa0f870bfc7d85801d289a4fb
Carter <carterinmotion@gmail.com>
2025-10-30 12:55:43 +0000

    Working cube with fisheye

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,35 +1,73 @@
+import * as THREE from 'three'
 import { useEffect, useRef, useState } from 'react';
-import * as THREE from 'three';
-import {Canvas, useFrame} from '@react-three/fiber'
+import {Canvas, ThreeElements, useFrame} from '@react-three/fiber'
+import { Backdrop, Environment, Fisheye } from "@react-three/drei"
 
-function Box(props) {
-				const meshRef = useRef()
-				const [hovered, setHover] = useState(false)
-				const [active, setActive] = useState(false)
-				useFrame((state, delta) => (meshRef.current.rotation.x += delta))
-
+function Box(props: ThreeElements['mesh']) {
+				const meshRef = useRef<THREE.Mesh>(null!)
+				useFrame((_, delta) => (meshRef.current.rotation.y += delta))
 				return (
 								<mesh
   								{...props}
   								ref={meshRef}
-  								scale={active ? 1.5 : 1}
-  								onClick={() => setActive(!active)}
-  								onPointerOver={() => setHover(true)}
-  								onPointerOut={() => setHover(false)}
 								>
   								<boxGeometry args={[1,1,1]} />
-  								<meshStandardMaterial color={hovered ? 'red': 'blue'} />
+  								<meshStandardMaterial color={'red'} />
 								</mesh>
 				)
 }
 
 export default function Home() {
+	const glRef = useRef<THREE.WebGLRenderer>()
+	const [darkMode, setDarkMode] = useState(isDarkMode())
+	function setBackgroundColor(darkMode: boolean){
+		if(!glRef.current) return
+		if(!window.matchMedia) return
+		const color = darkMode ? "#000000": "#ffffff"
+		glRef.current.setClearColor(color, 1)
+	}
+	function isDarkMode(){
+		if(!window.matchMedia) return false;
+		return window.matchMedia('(prefers-color-scheme: dark)').matches
+	}
+	useEffect(() => {
+		const listener = (e: MediaQueryListEvent) => setDarkMode(e.matches)
+		window.matchMedia(
+			'(prefers-color-scheme: dark)'
+		).addEventListener("change", listener)
+		return () => {
+			window.matchMedia(
+				'(prefers-color-scheme: dark)'
+			).removeEventListener("change", listener)
+		}
+	}, [])
+	useEffect(() => {
+		setBackgroundColor(darkMode)
+	}, [darkMode])
 	return (
 		<>
-			<div id='bigname' style={{ display: 'flex', width: '100%', flexGrow: 1, justifyContent: 'center', alignItems: 'start', flexDirection: 'column', paddingLeft: '30%' }}>
-				<Canvas>
-				  <ambientLight intensity={Math.PI / 2} />
-					<Box position={[0,0,0]} />
+			<div id='bigname' style={{ display: 'flex', width: '100%', flexGrow: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'column', paddingLeft: '30%' }}>
+				<Canvas 
+					flat 
+					onCreated={({gl}) => {
+						glRef.current = gl;
+						setBackgroundColor()
+					}}
+					style={{
+						zIndex: -1,
+						position: "absolute",
+						top: 0, bottom: 0, left: 0, right: 0,
+					}}
+				>
+					<Fisheye>
+						<ambientLight intensity={Math.PI / 2} />
+						<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+						<pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+						<Box 
+							position={[0,0,0]} 
+							scale={5}
+						/>
+					</Fisheye>
 				</Canvas>
 			</div>
 		</>
