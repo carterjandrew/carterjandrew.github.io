85fc1e3cc437c820a5f41d776cf4c642c63a541c
Carter <carterinmotion@gmail.com>
2025-11-11 12:00:54 +0000

    Added a mobile navbar, it's kinda jank but it works

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,10 @@
 import { motion } from "framer-motion"
-import { useEffect, useMemo, useState } from "react"
+import { MouseEventHandler, useEffect, useMemo, useRef, useState } from "react"
 import { NavLink, useLocation } from "react-router-dom"
 import { useTransition } from "../hooks/contextHooks"
 import Glitch from "./glitch"
+import { FaChevronDown } from "react-icons/fa"
+import { FaX } from "react-icons/fa6"
 
 type NavBarProps = {
 	style?: React.CSSProperties
@@ -45,6 +47,8 @@ const NavBarLink: React.FC<NavBarLinkProps> = ({link, delayedLocation, ...props}
 }
 
 const NavBar: React.FC<NavBarProps> = ({ style }) => {
+	const divRef = useRef<HTMLDivElement>()
+	const [isMobile, setIsMobile] = useState(false)
 	const location = useLocation()
 	const [delayedLocation,] = useState(location)
 	const [transition,] = useTransition()
@@ -69,6 +73,125 @@ const NavBar: React.FC<NavBarProps> = ({ style }) => {
 		}
 	}, [])
 
+	function isMobileScreen(){
+		if(!divRef.current) return;
+		setIsMobile(divRef.current.clientWidth < 600)
+	}
+	
+	function onDivRef(div: HTMLDivElement){
+		divRef.current = div
+		isMobileScreen()
+	}
+
+	useEffect(() => {
+		window.addEventListener('resize', isMobileScreen)
+		return () => window.removeEventListener('resize', isMobileScreen)
+	}, [])
+
+	function Links(){
+		return (
+			<>
+				{links.map((link) => (
+					<NavBarLink 
+						link={link} 
+						delayedLocation={delayedLocation}
+					/>
+				))}
+			</>
+		)
+	}
+
+	function NavbarLogo(){
+		return (
+				<Glitch>
+					<img
+						src="/logo.svg"
+						style={{
+							filter: darkMode ? "invert(100%)" : "",
+							width: "100%",
+							height: "50px",
+							objectFit: "contain"
+						}}
+					/>
+				</Glitch>
+		)
+	}
+
+	function MobileBar(){
+		const [showLinks, setShowLinks] = useState(false)
+
+		const onMobileBarClick: MouseEventHandler<HTMLDivElement>= () => {
+			setShowLinks(true)
+		}
+		return (
+			<>
+				<div id='mobile-nav-wrapper'>
+					<div style={{height: "2em", background: "var(--background-color)",}} />
+					<div
+						id='mobile-navbar'
+						onClick={onMobileBarClick}
+					>
+						<div style={{flex: 1}} />
+						<NavbarLogo />
+						<div style={{flex: 1, justifyContent: "end"}}>
+							<FaChevronDown />
+						</div>
+					</div>
+				</div>
+				<div
+					id='inner-navbar'
+					style={{
+						position: 'absolute',
+						flexDirection: 'column',
+						justifyContent: 'center',
+						display: showLinks ? "flex": "none",
+						top: 0,
+						left: 0,
+						width: '100vw',
+						height: '100vh',
+						padding: '5vw'
+					}}
+				>
+					<div style={{
+						flex: 1,
+						flexDirection: "row",
+						justifyContent: 'end',
+						width: '100%',
+						fontSize: '2em'
+					}}>
+						<div
+							onClick={() => setShowLinks(false)}
+						>
+							<FaX />
+						</div>
+					</div>
+					<Links />
+					<div style={{flex: 1}} />
+				</div>
+			</>
+		)
+	}
+
+	function DesktopBar(){
+		return (
+			<>
+				<div id='inner-navbar'>
+					<NavLink to="/" style={{border: "none"}}>
+						<NavbarLogo />
+					</NavLink>
+					<div style={{
+						flex: 1,
+						display: "flex",
+						gap: '--default-padding',
+						position: "relative",
+					}} >
+					</div>
+					<Links />
+				</div>
+			</>
+		)
+	}
+
 	return (
 		<motion.div
 			id='navbar'
@@ -76,32 +199,9 @@ const NavBar: React.FC<NavBarProps> = ({ style }) => {
 			animate={{ y: 0, transition: transition }}
 			exit={{ y: '-100%', transition: transition }}
 			style={style}
+			ref={onDivRef}
 		>
-			<div id='inner-navbar'>
-					<NavLink to="/" style={{border: "none"}}>
-						<Glitch>
-							<img
-								src="/logo.svg"
-								style={{
-									filter: darkMode ? "invert(100%)" : "",
-									width: "100%",
-									height: "50px",
-									objectFit: "contain"
-								}}
-							/>
-						</Glitch>
-					</NavLink>
-				<div style={{
-					flex: 1,
-					display: "flex",
-					gap: '--default-padding',
-					position: "relative",
-				}} >
-				</div>
-				{links.map((link) => (
-					<NavBarLink link={link} delayedLocation={delayedLocation} />
-				))}
-			</div>
+			{isMobile ? (<MobileBar />): (<DesktopBar />)}
 		</motion.div>
 	)
 }
